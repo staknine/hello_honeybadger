fc607988dc271d76ab5e226bf490ce0bd1fb613b
StakNine <staknine@protonmail.com>
2021-10-23 15:00:50 -0700

    Add error and config for development

--- lib/hello_honeybadger_web/controllers/page_controller.ex
+++ lib/hello_honeybadger_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule HelloHoneybadgerWeb.PageController do
   use HelloHoneybadgerWeb, :controller
 
   def index(conn, _params) do
+    raise ArgumentError, message: "We are testing error reporting with Honeybadger"
     render(conn, "index.html")
   end
 end
